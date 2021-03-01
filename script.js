662e8c4ec97ff3dbedbf363d226bac5defff3bc3
Mariya Shulyak <shulyakmariya@yandex.ru>
2021-03-01 14:05:11 +0300

    Почему при выборе уровня middle не добавляется класс middle_game?

--- script.js
+++ script.js
@@ -2,8 +2,7 @@ const level = document.querySelectorAll('.item_level');
 const start = document.getElementById('start');
 const bugCard = document.createElement('img');
 const menu = document.getElementById('wrap')
-const container = document.getElementById ('container')
-bugCard.src = 'img/bug.png';
+let numberCards = 3;
 
 
 // выбор уровня, добавление ромба
@@ -13,43 +12,49 @@ level.forEach(function(element) {
             element.classList.remove('item_level_active')
                   })
         element.classList.add('item_level_active');
-      });
+      })
+    });
 
 
-//Старт игры.
-function startGame() {
-	container.classList.add('hidden');
-  //Переход к карточному столу и создание карт выбранного уровня
-	switch (level) {
+start.addEventListener('click', function(){
+  container.classList.add('hidden');
+  menu.classList.remove('hidden');
+  let levelChoice = document.querySelector('.item_level_active').getAttribute('id');
+  switch (levelChoice) {
     case 'simple':
           numberCards = 3;
          break;
 
         case 'middle':
           numberCards = 6;
-
         break;
 
          case 'hight':
           numberCards = 9;
-
         break;
-	};
+  }
   createCard = () => {
      let newCards = document.createElement('div');
      newCards.classList = 'card_back';
      menu.append(newCards);
      switch (numberCards) {
+      case 3:
+       menu.classList.toggle("simple_game");
+       break;
+
+       case 6:
+       menu.classList.toggle("middle_game");
+       break;
+
        case 9:
         menu.classList.toggle("hight_game");
         break;
       }
-      createCard();
-  }
-    start.addEventListener('click', startGame());
-
-
-// Рандомная Замена карты с gameOver на bugCard
-	const random = Math.floor(Math.random());
-};
-});
+      function random() {
+          return Math.round(Math.random());
+        }
+      };
+      for (let i = 0; i < numberCards; i++ ) {
+       createCard()
+     }
+    });
