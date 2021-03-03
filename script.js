1e3d8e5487486d3446a36cf5750fac4282cd7212
Mariya Shulyak <shulyakmariya@yandex.ru>
2021-03-03 15:47:03 +0300

    Не понимаю как сделать?

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const level = document.querySelectorAll('.item_level');
 const start = document.getElementById('start');
 const menu = document.getElementById('wrap');
 const  container = document.getElementById('container');
-
+let numberCards = 3;
 
 // выбор уровня, добавление ромба
 level.forEach(function(element) {
@@ -41,23 +41,37 @@ start.addEventListener('click', function(){
   function random() {
       return Math.round(Math.random());
     }
-  let numberClick = 0;
-      newCards.addEventListener ('click',   function showHide() {
-        ++numberClick;
-        if (numberClick % 2 !== 0) {
-        random () ===0 ? newCards.classList.add('card_front') : newCards.classList.add('card_bug')
-        newCards.classList.remove('card_back');
-      } else {
-          newCards.classList.add('card_front')
-           container.classList.toggle('hidden');
-   		     menu.classList.toggle('hight_game');
-           let cards = document.querySelectorAll('.card_front');
-   			cards.forEach(el => el.remove());
-      }
-    });
-  }
 
-         for (let i = 0; i < numberCards; i++ ) {
-          createCard(numberCards);
-        }
+      let click =0 ;
+      if (click %2 !== 0){
+      newCards.addEventListener ('click', function flip (event) {
+          random () ===0 ? newCards.classList.add('card_front') : newCards.classList.add('card_bug')
+          newCards.classList.remove('card_back');
+        })
+      } else {
+      let block = document.createElement('div');
+   		  block.classList = 'block';
+   		  document.body.append(block);
+        block.addEventListener ('click',function end () {
+          window.location.reload();
 })
+}
+          /*  let numberClick = 0;
+                newCards.addEventListener ('click',   function showHide() {
+                  ++numberClick;
+                  if (numberClick % 2 !== 0) {
+                  random () ===0 ? newCards.classList.add('card_front') : newCards.classList.add('card_bug')
+                  newCards.classList.remove('card_back');
+                } else {
+                    newCards.classList.add('card_front')
+                     container.classList.toggle('hidden');
+             		     menu.classList.toggle('hight_game');
+                     let cards = document.querySelectorAll('.card_front');
+             			cards.forEach(el => el.remove());
+                }
+              });*/
+}
+for (let i = 0; i < numberCards; i++ ) {
+   createCard(numberCards);
+ }
+});
