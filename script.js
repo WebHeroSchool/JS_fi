5a4982b171b98e7c882a727b54ad78a1efb06572
Mariya Shulyak <shulyakmariya@yandex.ru>
2021-03-05 09:48:04 +0300

    Я не понимаю почему не работает событие

--- script.js
+++ script.js
@@ -45,54 +45,22 @@ start.addEventListener('click', function(){
      createCard (numberCards);
 
 
-
-
-    /*const card= document.querySelectorAll('.card');
-      let arrCards = function(card) {
-  return arrCards.push(card);
-};
-
-      /*let click =0 ;
-      if (click %2 !== 0){
-      newCards.addEventListener ('click', function flip (event) {
-          random () ===0 ? newCards.classList.add('card_front') : newCards.classList.add('card_bug')
-          newCards.classList.remove('card_back');
-        })
-      } else {
-      let block = document.createElement('div');
-   		  block.classList = 'block';
-   		  document.body.append(block);
-        block.addEventListener ('click',function end () {
-          window.location.reload();
-})
-}*/
-
 // рандомное расположение  и клики по картам.
 function random() {
     return Math.round(Math.random());
   }
-           let numberClick = 0;
 
-  let arrCards = [document.querySelectorAll('card')].forEach(item => {
-                item.addEventListener('click', function(){
-                  ++numberClick;
-                  if (numberClick %2 !== 0) {
-                  random () ===0 ? item.classList.add('card_front') : item.classList.add('card_bug')
-                  item.classList.remove('card_back');
-                } else {
-                    /* container.classList.toggle('hidden');
-             		     menu.classList.toggle('hight_game');
-                     let cards = document.querySelectorAll('.card');
-             			cards.forEach(el => el.remove());*/
-                    window.location.reload();
-                }
-              });
-
-})
-
-
-/*for (let i = 0; i < numberCards; i++ ) {
-   createCard();
-}*/
+  let numberClick = 0;
+  let cards = document.querySelectorAll('card');
+  cards.forEach(item =>
+        item.addEventListener('click', function(){
+        ++numberClick;
+        if (numberClick %2 !== 0) {
+        random () ===0 ? item.classList.add('card_front') : item.classList.add('card_bug')
+        item.classList.remove('card_back');
+        } else {
+        window.location.reload();
+        }
+    }));
 };
 });
